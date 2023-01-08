2b9c3ee0ec9a246b96ff10b886e7beaf4060fb49
Erven Idjad <ervenidjad12@gmail.com>
2023-01-09 03:47:12 +0800

    .

--- homeIncludes/header.php
+++ homeIncludes/header.php
@@ -7,10 +7,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <link rel="stylesheet" href="style.css" />
     <link rel="stylesheet" href="style2.css" />
     <script src="script.js" defer></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     <title>
         <?php echo $page_title; ?>
     </title>
--- repair.php
+++ repair.php
@@ -103,7 +103,7 @@ include_once('homeIncludes/header.php');
                 </div>
         </form>
     </div>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
