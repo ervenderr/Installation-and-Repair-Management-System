e0b73f548d926432f2216a1d36619dd8c904d355
Erven Idjad <ervenidjad12@gmail.com>
2023-02-16 23:57:30 +0800

    Update transaction.php

--- pages/transaction.php
+++ pages/transaction.php
@@ -176,6 +176,129 @@ if (!isset($_SESSION['user_type']) || $_SESSION['user_type'] != 'admin'){
     });
     </script>
 
+    <script>
+    const form = document.querySelector('.form-sample');
+    const fname = form.querySelector('input[name="fname"]');
+    const lname = form.querySelector('input[name="lname"]');
+    // const email = form.querySelector('input[name="email"]');
+    const phone = form.querySelector('input[name="phone"]');
+    const address = form.querySelector('input[name="address"]');
+    const etype = form.querySelector('select[name="etype"]');
+    // const electrician = form.querySelector('select[name="electrician"]');
+    const defective = form.querySelector('input[name="defective"]');
+    const shipping = form.querySelector('select[name="shipping"]');
+    const date = form.querySelector('input[name="date"]');
+    const completed = form.querySelector('input[name="completed"]');
+    const payment = form.querySelector('input[name="payment"]');
+
+    form.addEventListener('submit', (event) => {
+        let error = false;
+
+        if (fname.value === '') {
+            fname.nextElementSibling.innerText = 'Please enter first name';
+            error = true;
+        } else if (!/^[A-Z][a-z]*$/.test(fname.value)) {
+            fname.nextElementSibling.innerText = 'First name should be capitalized';
+            error = true;
+        } else {
+            fname.nextElementSibling.innerText = '';
+        }
+
+        if (lname.value === '') {
+            lname.nextElementSibling.innerText = 'Please enter last name';
+            error = true;
+        } else if (!/^[A-Z][a-z]*$/.test(lname.value)) {
+            lname.nextElementSibling.innerText = 'Last name should be capitalized';
+            error = true;
+        } else {
+            lname.nextElementSibling.innerText = '';
+        }
+
+        // if (email.value === '') {
+        //     email.nextElementSibling.innerText = 'Please enter your email';
+        //     error = true;
+        // } else {
+        //     email.nextElementSibling.innerText = '';
+        // }
+
+        if (phone.value === '') {
+            phone.nextElementSibling.innerText = 'Please enter phone number';
+            error = true;
+        } else if (!/^\d{11}$/.test(phone.value)) {
+            phone.nextElementSibling.innerText = 'Please enter a valid 11-digit phone number';
+            error = true;
+        } else {
+            phone.nextElementSibling.innerText = '';
+        }
+
+        if (address.value === '') {
+            address.nextElementSibling.innerText = 'Please enter address';
+            error = true;
+        } else if (!/^[a-zA-Z0-9\s,'-]*$/.test(address.value)) {
+            address.nextElementSibling.innerText = 'Please enter a valid address';
+            error = true;
+        } else {
+            address.nextElementSibling.innerText = '';
+        }
+
+        if (etype.value === 'None') {
+            etype.nextElementSibling.innerText = 'Please select an electronic type';
+            error = true;
+        } else {
+            etype.nextElementSibling.innerText = '';
+        }
+
+        // if (electrician.value === 'None') {
+        //     electrician.nextElementSibling.innerText = 'Please select an electrician';
+        //     error = true;
+        // } else {
+        //     electrician.nextElementSibling.innerText = '';
+        // }
+
+        if (defective.value === '') {
+            defective.nextElementSibling.innerText = 'Please enter your defective';
+            error = true;
+        } else {
+            defective.nextElementSibling.innerText = '';
+        }
+
+        if (shipping.value === 'None') {
+            shipping.nextElementSibling.innerText = 'Please select a shipping option';
+            error = true;
+        } else {
+            shipping.nextElementSibling.innerText = '';
+        }
+
+        if (date.value === '') {
+            date.nextElementSibling.innerText = 'Please select a date';
+            error = true;
+        } else {
+            date.nextElementSibling.innerText = '';
+        }
+
+        if (completed.value === '') {
+            completed.nextElementSibling.innerText = 'Please select a completion date';
+            error = true;
+        } else {
+            completed.nextElementSibling.innerText = '';
+        }
+
+        if (payment.value === '') {
+            payment.nextElementSibling.innerText = 'Please enter a payment amount';
+            error = true;
+        } else {
+            payment.nextElementSibling.innerText = '';
+        }
+
+        if (error) {
+            event.preventDefault(); // Prevent form submission if there are errors
+        } else {
+            // Submit form to server if there are no errors
+            // You can use AJAX to submit the form asynchronously, or just let it submit normally
+        }
+    });
+</script>
+
 </body>
 
 </html>
