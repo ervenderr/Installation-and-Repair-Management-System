fa2623b723016623912f8bf772ffc20cf65cce5d
Erven Idjad <ervenidjad12@gmail.com>
2023-02-16 23:58:28 +0800

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -69,18 +69,7 @@ if (!isset($_SESSION['user_type']) || $_SESSION['user_type'] != 'admin'){
                 </div>
               </div>
             </div>
-            <div class="row">
-              <div class="col-md-4 stretch-card grid-margin">
-                <div class="card bg-gradient-warning card-img-holder text-white">
-                  <div class="card-body">
-                    <img src="../assets/images/dashboard/circle.svg" class="card-img-absolute" alt="circle-image" />
-                    <h4 class="font-weight-normal mb-3">Pending Repair Request <i class="mdi mdi-chart-line mdi-24px float-right"></i>
-                    </h4>
-                    <h2 class="mb-5">2</h2>
-                    <h6 class="card-text">Increased by 60%</h6>
-                  </div>
-                </div>
-              </div>
+            
               <div class="col-md-4 stretch-card grid-margin">
                 <div class="card bg-gradient-primary card-img-holder text-white">
                   <div class="card-body">
