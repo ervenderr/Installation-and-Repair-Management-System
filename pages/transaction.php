0adc2a926f0ad9d6350073501a9d6ca58daf4d52
Erven Idjad <ervenidjad12@gmail.com>
2023-02-17 02:06:54 +0800

    Update transaction.php

--- pages/transaction.php
+++ pages/transaction.php
@@ -1,12 +1,12 @@
 <?php
-include_once('../admin_includes/header.php');
-include_once('../homeincludes/dbconfig.php');
-
 session_start();
 if (!isset($_SESSION['user_type']) || $_SESSION['user_type'] != 'admin'){
     header('location: ../login/login.php');
   }
 ?>
+
+include_once('../admin_includes/header.php');
+include_once('../homeincludes/dbconfig.php');
 
 <body>
     <div class="container-scroller">
