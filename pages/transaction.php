bd5d38e153e491a452f62b19de1eb60d24dbd22f
Erven Idjad <ervenidjad12@gmail.com>
2023-02-17 09:57:51 +0800

    Update transaction.php

--- pages/transaction.php
+++ pages/transaction.php
@@ -6,7 +6,7 @@ if (!isset($_SESSION['user_type']) || $_SESSION['user_type'] != 'admin'){
 
 
 include_once('../admin_includes/header.php');
-ini_set('display_errors', 1); error_reporting(E_ALL);
+include_once('../homeIncludes/dbconfig.php');
 ?>
 
 <body>
@@ -73,7 +73,6 @@ ini_set('display_errors', 1); error_reporting(E_ALL);
                                             </thead>
                                             <tbody>
                                                 <?php
-                                                    include_once('../homeIncludes/dbconfig.php');
                                                     $query = "SELECT rprq.id, rprq.transaction_code, customer.fname, customer.lname, rprq.status, rprq.date_req
                                                         FROM rprq
                                                         JOIN customer ON rprq.Cust_id = customer.Cust_id
