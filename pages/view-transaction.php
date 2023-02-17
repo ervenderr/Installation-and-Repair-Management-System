7f84f4f59f4aedb357913dc622726f9662fe32f0
Erven Idjad <ervenidjad12@gmail.com>
2023-02-17 10:11:00 +0800

    Update view-transaction.php

--- pages/view-transaction.php
+++ pages/view-transaction.php
@@ -1,6 +1,6 @@
 <?php
 include_once('../admin_includes/header.php');
-include_once('../homeincludes/dbconfig.php');
+include_once('../homeIncludes/dbconfig.php');
 
 $rowid = $_GET['rowid'];
 $tcode = $_GET['transaction_code'];
