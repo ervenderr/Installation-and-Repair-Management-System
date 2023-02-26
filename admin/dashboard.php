53be74700697cfc3f74666478afb333e87a5117f
Erven Idjad <ervenidjad12@gmail.com>
2023-02-27 02:08:46 +0800

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-require_once '../admin_includes/header.php';
-require_once('../homeincludes/dbconfig.php');
+include_once('../admin_includes/header.php');
+include_once('../homeincludes/dbconfig.php');
 
 if (!isset($_SESSION['user_type']) || $_SESSION['user_type'] != 'admin'){
     header('location: ../login/login.php');
