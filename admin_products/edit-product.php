433f5805f2232920e3fc6aad7e21d96a4d455219
Erven Idjad <ervenidjad12@gmail.com>
2023-02-27 10:36:14 +0800

    Update edit-product.php

--- admin_products/edit-product.php
+++ admin_products/edit-product.php
@@ -34,8 +34,8 @@ if (mysqli_num_rows($result) > 0) {
                     <div class="page-header">
                         <h3 class="page-title">
                             <span class="page-title-icon text-white me-2">
-                                <i class="fas fa-users menu-icon"></i>
-                            </span> Technicians <span class="bread">/ Update technician info</span>
+                                <i class="fas fa-box menu-icon"></i>
+                            </span> Products <span class="bread">/ Update product info</span>
                         </h3>
                         <nav aria-label="breadcrumb">
                             <ul class="breadcrumb">
