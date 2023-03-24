effa1d673fae039530e34774b18f7bde305df913
Erven Idjad <ervenidjad12@gmail.com>
2023-03-24 09:57:32 +0800

    Update transactions.php

--- adminServices/transactions.php
+++ adminServices/transactions.php
@@ -145,8 +145,8 @@ if (!isset($_SESSION['user_type']) || $_SESSION['user_type'] != 'admin'){
                                                         echo '<a class="icns" href="edit-transactions.php?transaction_code=' . $row['transaction_code'] . '&rowid=' . $row['sreq_id'] . '">';
                                                         echo '<i class="fas fa-edit text-success view-account" data-rowid="' . $row['sreq_id'] . '"></i>';
                                                         echo '</a>';
-                                                        echo '<a class="icns" href="delete-transactions.php?transaction_code=' . $row['transaction_code'] . '&rowid=' . $row['sreq_id'] . '">';
-                                                        echo '<i class="fas fa-trash-alt text-danger view-account" data-rowid="' . $row['sreq_id'] . '"></i> onclick="return confirm(\'Are you sure you want to delete this product?\')">';
+                                                        echo '<a class="icns" href="delete-transaction.php?transaction_code=' . $row['transaction_code'] . '&rowid=' .  $row['sreq_id'] . '" onclick="return confirm(\'Are you sure you want to delete this product?\')">';
+                                                        echo '<i class="fas fa-trash-alt text-white view-account delete" data-rowid="' .  $row['sreq_id'] . '"></i>';
                                                         echo '</a>';
                                                         echo '</td>';
                                                         echo '</tr>';
